46b3a29537cb23839f92e350546c190b51e1c74f
Jonhh1 <jonathanvalecio@gmail.com>
2021-11-14 19:35:58 -0300

    Verificador de idade

--- aula_12ex/ex014/script.js
+++ aula_12ex/ex014/script.js
@@ -1,1 +1,21 @@
-alert('Olá')
+//alert('Olá')
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = 23//data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora<12) {
+         
+        img.src = 'fotomanha.png' 
+        document.body.style.background = '#f1b671'    
+    } else if (hora>=12 && hora<18){        
+        img.src = 'fototarde.png'
+        document.body.style.background = '#eb794b'      
+        
+    }else{        
+        img.src = 'fotonoite.png' 
+        document.body.style.background = '#04537e'
+        
+    }
+}
