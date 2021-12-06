36bdfea8a73624a3d6c0a41ccdf9c636c18b09f9
Jonh <jonathanvalecio@gmail.com>
2021-12-05 21:38:09 -0300

    Tabuada

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,21 @@
+//alert('Olá')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    
+
+    if (num.value.length != 0) {
+        let n = Number(num.value)
+        tab.innerHTML=''
+        for (let c = 1; c <= 10; c++){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild (item)
+         
+        }
+
+    } else {
+        alert('Porfavor, digite um numero!')        
+    }
+}
